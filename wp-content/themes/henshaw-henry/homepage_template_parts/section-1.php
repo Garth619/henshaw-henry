3c50b9f70a6a47479f24ec4521a64ef6c3937597
Garrett Cullen <Garth619@users.noreply.github.com>
2017-11-14 16:34:19 -0800

    push

--- wp-content/themes/henshaw-henry/homepage_template_parts/section-1.php
+++ wp-content/themes/henshaw-henry/homepage_template_parts/section-1.php
@@ -1,1 +1,19 @@
-<section class="section_one"></section>
+<section class="section_one">
+	
+	<div class="inner_section_one">
+		
+		<span class="sec_one_header">Experienced Attorneys Exceptional Results</span><!-- sec_one_header -->
+		
+		<ul class="subheader_bullets">
+			
+			<li>Responsive</li>
+			<li>Tenacious</li>
+			<li>Friendly</li>
+			<li>Caring</li>
+			<li>knowledgeable</li>
+			
+		</ul><!-- bullets -->
+		
+	</div><!-- inner_section_one -->
+	
+</section><!-- section_one -->
