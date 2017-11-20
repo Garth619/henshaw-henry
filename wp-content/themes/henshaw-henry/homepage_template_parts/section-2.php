80d41abfa2bb89efe0db8e391a8d1bd245d66929
Garrett Cullen <Garth619@users.noreply.github.com>
2017-11-20 12:52:56 -0800

    push

--- wp-content/themes/henshaw-henry/homepage_template_parts/section-2.php
+++ wp-content/themes/henshaw-henry/homepage_template_parts/section-2.php
@@ -1,1 +1,52 @@
-<section id="section_two"></section><!-- section_two -->
+<section id="section_two">
+	
+	<div class="testi_wrapper">
+		
+		<div class="testi_inner_wrapper">
+			
+			<span class="large_header">What our<br/> clients have said</span><!-- large_header -->
+			
+			<div class="testi_slideshow">
+				
+				<div class="testi_slide">
+				
+				<div class="testi_content">
+					
+					<p>“Tough situations are never fun but you can count on Henshaw & Henry to ease the process! I can't stress how much Henshaw & Henry has helped me through my difficult time! I would go through it again, without fear, as long as Henshaw & Henry is on my side!</p>
+
+<p>After being turned down by five law firms, Henshaw & Henry accepted my case with confidence and assertiveness. I had the pleasure to work with Timothy D. Henry, who was very professional, clear, patient, resourceful, and knowledgeable. This in turn, gave me peace of mind and assurance.”</p>
+
+				
+				<div class="testi_name_wrapper">
+					
+					<svg class="testi_graphic" data-name="Layer 1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 27.88 5.83">
+						
+						<path class="testi_graphic-cls-1" d="M27.7.5S26,.5,23.4.4,17.3.3,13.9.2C12.2.2,10.4.2,8.8.1H4.4C3.1,0,2,0,1.3,0,.9,0,.6-.1.4.1A.78.78,0,0,0,.1.5a.85.85,0,0,0,0,.7,1.73,1.73,0,0,0,.3.4,2.22,2.22,0,0,0,.9.1c.8,0,1.8,0,3.1-.1s2.8,0,4.4-.1,3.3-.1,5.1-.1c3.5-.1,6.9-.2,9.5-.3S27.7,1,27.7,1s.2-.1.2-.3A.22.22,0,0,0,27.7.5Z" transform="translate(-0.03 0.03)"/>
+						<path class="testi_graphic-cls-1" d="M27.7,4.5s-1.7,0-4.3-.1-6.1-.1-9.5-.2c-1.7,0-3.5,0-5.1-.1H1.3c-.4,0-.7-.1-.9,0a1.73,1.73,0,0,0-.3.4,1.1,1.1,0,0,0,0,.8,1.73,1.73,0,0,0,.3.4,2.22,2.22,0,0,0,.9.1c.8,0,1.8,0,3.1-.1s2.8,0,4.4-.1,3.3-.1,5.1-.1c3.5-.1,6.9-.2,9.5-.3S27.7,5,27.7,5s.2-.1.2-.3A.22.22,0,0,0,27.7,4.5Z" transform="translate(-0.03 0.03)"/>
+					
+					</svg>
+					
+					<span class="testi_name">D.P. - Past Client</span><!-- testi_name -->
+					
+					
+				</div><!-- testi_name_wrapper -->
+			
+
+				</div><!-- testi_slide -->
+					
+				</div><!-- testi_content -->
+				
+			</div><!-- testi_slideshow -->
+			
+		</div><!-- testi_inner_wrapper -->
+		
+	</div><!-- testi_wrapper -->
+	
+	<div class="reviews_wrapper">
+		
+		
+		
+	</div><!-- reviews_wrapper -->
+	
+	
+</section><!-- section_two -->
