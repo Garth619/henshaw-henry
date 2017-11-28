6b5ac9d5ac38392f0d057da450a8905224e9542c
Garrett Cullen <Garth619@users.noreply.github.com>
2017-11-28 14:53:12 -0800

    push

---
+++ wp-content/themes/henshaw-henry/homepage_template_parts/section-4.php
@@ -0,0 +1,91 @@
+<section class="section_four">
+	
+	
+	<div class="pa_wrapper">
+		
+		
+		
+		
+			<div class="single_pa">
+				
+				<a class="" href="">
+			
+				<span class="single_pa_title">Civil<br/> Litigation</span><!-- single_pa_title -->
+			
+				<div class="pa_dots_wrapper">
+				
+					<div class="pa_dot"></div><!-- pa_dot -->
+					<div class="pa_dot"></div><!-- pa_dot -->
+				
+				</div><!-- pa_dots_wrapper -->
+				
+				<span class="pa_view_more">View More</span><!-- view_more -->
+			
+			<div class="blue_image"></div><!-- blue_image -->
+			
+			<div class="orange_image"></div><!-- blue_image -->
+			
+			
+			</a>
+			
+			
+		</div><!-- single_pa -->
+		
+		
+		<div class="single_pa">
+			
+				<a class="" href="">
+			
+				<span class="single_pa_title">Personal<br/> Injury</span><!-- single_pa_title -->
+			
+				<div class="pa_dots_wrapper">
+				
+					<div class="pa_dot"></div><!-- pa_dot -->
+					<div class="pa_dot"></div><!-- pa_dot -->
+				
+				</div><!-- pa_dots_wrapper -->
+				
+				<span class="pa_view_more">View More</span><!-- view_more -->
+			
+			<div class="blue_image"></div><!-- blue_image -->
+			
+			<div class="orange_image"></div><!-- blue_image -->
+			
+			
+			</a>
+			
+			
+		</div><!-- single_pa -->
+		
+		<div class="single_pa">
+			
+				<a class="" href="">
+			
+				<span class="single_pa_title">Estate<br/> Planning</span><!-- single_pa_title -->
+			
+				<div class="pa_dots_wrapper">
+				
+					<div class="pa_dot"></div><!-- pa_dot -->
+					<div class="pa_dot"></div><!-- pa_dot -->
+				
+				</div><!-- pa_dots_wrapper -->
+				
+				<span class="pa_view_more">View More</span><!-- view_more -->
+			
+			<div class="blue_image"></div><!-- blue_image -->
+			
+			<div class="orange_image"></div><!-- blue_image -->
+			
+			
+			</a>
+			
+			
+		</div><!-- single_pa -->
+		
+		
+
+		
+	</div><!-- pa_wrapper -->
+	
+	
+</section><!-- section_four -->
