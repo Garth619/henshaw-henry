c8b077bf52df0aab1a9091f1fc5259c615c01a7c
Garrett Cullen <Garth619@users.noreply.github.com>
2017-11-29 16:30:25 -0800

    push

--- wp-content/themes/henshaw-henry/homepage_template_parts/section-6.php
+++ wp-content/themes/henshaw-henry/homepage_template_parts/section-6.php
@@ -1,5 +1,31 @@
 <section class="section_six">
 	
+	<div class="section_six_img_wrapper">
+		
+		<div class="section_six_img_inner"></div><!-- section_six_img_inner -->
+		
+	</div><!-- section_six_img_wrapper -->
 	
+	<div class="section_six_selling_wrapper">
+		
+		<div class="selling_point_wrapper">
+			
+			<a class="sec_six_why_button" href="">Why Choose Our Firm?</a><!-- sec_six_why_button -->
+			
+			<div class="selling_point_slideshow">
+				
+				<div class="selling_point_slide">
+					
+					<img class="sp_icon" src="<?php bloginfo('template_directory');?>/images/sp-icon-01.svg"/>
+					
+					<span class="sp_slide_title">Supportive</span><!-- sp_slide_title -->
+					
+				</div><!-- selling_point_slide -->
+				
+			</div><!-- selling_point_slideshow -->
+			
+		</div><!-- selling_point_wrapper -->
+		
+	</div><!-- section_six_selling_wrapper -->
 	
 </section><!-- section_six -->
