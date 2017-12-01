5d33a3525c2696b94af0c5ba1952212db692c114
Garrett Cullen <Garth619@users.noreply.github.com>
2017-12-01 10:03:07 -0800

    push

--- wp-content/themes/henshaw-henry/footer.php
+++ wp-content/themes/henshaw-henry/footer.php
@@ -9,10 +9,33 @@
 			
 			<span class="top_footer_content">Henshaw & Henry, PC takes an analytic approach to your personal needs, contact us today.</span><!-- top_footer_content -->
 			
+			
+			<div class="location_col">
+				
+				<span class="our_office">Our Office</span><!-- our_office -->
+				
+				<a class="address" href="">1871 The Alameda<br/> Suite 333<br/> San Jose, CA 95126</a>
+				
+				<a class="directions" href="">Get Directions</a>
+				
+				<a class="tel" href="tel:(408) 533-1075">(408) 533-1075</a>
+				
+			</div><!-- location_col -->
+			
+			<div class="location_col">
+				
+				<p>We are experienced in the common problems that individuals and businesses face. While each and every client has a unique set of problems, we are confident that we have the expertise to resolve your legal issues.</p>
+
+				<blockquote>We enjoy building relationships with our clients and understand the problems they face every day. </blockquote>
+
+				<p>Too often people are afraid to contact their lawyer. Our clients appreciate that we listen to their goals and objectives during the entire representation. In doing so, we are able to advise them towards the most desired legal conclusion.</p>
+				
+			</div><!-- location_col -->
+			
+		
 		</div><!-- inner_top_footer -->
 		
 	</div><!-- top_footer -->
-	
 	
 	
 </footer>
