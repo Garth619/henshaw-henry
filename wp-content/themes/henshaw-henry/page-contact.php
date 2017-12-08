896b759b8bf623060372eb5bc4029989f1766c84
Garrett Cullen <Garth619@users.noreply.github.com>
2017-12-08 09:53:17 -0800

    push

---
+++ wp-content/themes/henshaw-henry/page-contact.php
@@ -0,0 +1,67 @@
+<?php 
+	
+	/* Template Name: Contact */
+	
+	get_header(); ?>
+
+
+
+<div class="internal_main">
+	
+	
+	<div class="one_column">
+		
+		
+		<div class="contact_page_wrapper">
+			
+			
+			<img class="contact_svg" src="<?php bloginfo('template_directory');?>/images/contact.svg"/>
+			
+			<span class="contact_decription">Henshaw & Henry, PC takes an analytic approach to your personal needs, contact us today.</span><!-- contact_decription -->
+			
+			
+			
+			<div class="contact_col">
+				
+				<span class="contact_page_office_title">our office</span><!-- contact_page_office_title -->
+				
+				<a class="contact_page_address" href="">1871 The Alameda<br/> Suite 333<br/> San Jose, CA 95126</a>
+				
+				<a class="contact_page_directions" href="">Get Directions</a>
+				
+				<a class="contact_page_tel" href="tel:(408) 533-1075">(408) 533-1075</a>
+				
+				
+				
+			</div><!-- contact_col -->
+			
+			<div class="contact_col">
+				
+				<iframe class="contact_map" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3172.1181149538293!2d-121.92477508469473!3d37.33971227984084!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x808fcb729061e121%3A0x6cf5bafd7fbae6ea!2s1871+The+Alameda+%23333%2C+San+Jose%2C+CA+95126!5e0!3m2!1sen!2sus!4v1512754386893" frameborder="0" style="border:0" allowfullscreen></iframe>
+			
+				
+			</div><!-- contact_col -->
+			
+			
+		</div><!-- contact_page_wrapper -->
+		
+		
+	</div><!-- one_column -->
+	
+	
+</div><!-- internal_main -->
+
+
+
+<?php // get_template_part( 'loop', 'index' );?>
+
+
+
+
+<?php //use get_template_part( 'loop', 'page' ) if needed ?>
+
+	
+
+
+
+<?php get_footer(); ?>
