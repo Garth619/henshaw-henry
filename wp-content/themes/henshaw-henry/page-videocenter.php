a3b846c0e780262a58ac41ca99c8ae722b55a201
Garrett Cullen <Garth619@users.noreply.github.com>
2017-12-08 14:59:09 -0800

    push

---
+++ wp-content/themes/henshaw-henry/page-videocenter.php
@@ -0,0 +1,349 @@
+<?php 
+	
+	/* Template Name: Video Center */
+	
+	get_header(); ?>
+
+
+
+<div class="internal_main">
+	
+	
+	<div class="one_column">
+		
+		
+		<div class="video_center_wrapper content">
+			
+			
+			<h1 class="center">Video Center</h1><!--- smaller_internal_header -->
+			
+			
+			<div class="videos">
+				
+				
+				<div class="single_video">
+					
+					<div class="single_play_wrapper">
+							
+							<svg class="video_center_play" data-name="Layer 1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 120.2 120.2">
+								
+								<defs><style>.video_center-cls-1,.video_center-cls-3{fill:#fff;}.video_center-cls-1{opacity:0.2;isolation:isolate;}.video_center-cls-2{fill:none;stroke:#fff;stroke-miterlimit:10;stroke-width:3px;}</style></defs>
+								
+								<title>circular-play-button</title>
+								
+								<circle class="video_center-cls-1" cx="60.1" cy="60.1" r="58.6"/>
+								
+								<circle class="video_center-cls-2" cx="60.1" cy="60.1" r="58.6"/>
+								
+								<polygon class="video_center-cls-3" points="48.8 44.3 63 52.5 77.3 60.8 63.1 69 48.8 77.3 48.8 60.8 48.8 44.3"/>
+								
+							</svg>
+							
+						</div><!-- single_play_wrapper -->
+					
+					
+					<div class="single_video_inner wistia_embed wistia_async_m8e47nd9p4 popover=true popoverContent=thumbnail">
+						
+						
+						
+					</div><!-- single_video_inner -->
+					
+					<span class="grey_line"></span>
+					
+					<span class="video_title wistia_embed wistia_async_m8e47nd9p4 popover=true popoverContent=html">Lorem Ipsum Amet</span><!-- video_title -->
+					
+					
+				</div><!-- single_video -->
+				
+				
+				
+				<div class="single_video">
+					
+					<div class="single_play_wrapper">
+							
+							<svg class="video_center_play" data-name="Layer 1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 120.2 120.2">
+								
+								<defs><style>.video_center-cls-1,.video_center-cls-3{fill:#fff;}.video_center-cls-1{opacity:0.2;isolation:isolate;}.video_center-cls-2{fill:none;stroke:#fff;stroke-miterlimit:10;stroke-width:3px;}</style></defs>
+								
+								<title>circular-play-button</title>
+								
+								<circle class="video_center-cls-1" cx="60.1" cy="60.1" r="58.6"/>
+								
+								<circle class="video_center-cls-2" cx="60.1" cy="60.1" r="58.6"/>
+								
+								<polygon class="video_center-cls-3" points="48.8 44.3 63 52.5 77.3 60.8 63.1 69 48.8 77.3 48.8 60.8 48.8 44.3"/>
+								
+							</svg>
+							
+						</div><!-- single_play_wrapper -->
+					
+					
+					<div class="single_video_inner wistia_embed wistia_async_m8e47nd9p4 popover=true popoverContent=thumbnail">
+						
+						
+						
+					</div><!-- single_video_inner -->
+					
+					<span class="grey_line"></span>
+					
+					<span class="video_title wistia_embed wistia_async_m8e47nd9p4 popover=true popoverContent=html">Lorem Ipsum Amet</span><!-- video_title -->
+					
+					
+				</div><!-- single_video -->
+				
+				
+				
+				<div class="single_video">
+					
+					<div class="single_play_wrapper">
+							
+							<svg class="video_center_play" data-name="Layer 1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 120.2 120.2">
+								
+								<defs><style>.video_center-cls-1,.video_center-cls-3{fill:#fff;}.video_center-cls-1{opacity:0.2;isolation:isolate;}.video_center-cls-2{fill:none;stroke:#fff;stroke-miterlimit:10;stroke-width:3px;}</style></defs>
+								
+								<title>circular-play-button</title>
+								
+								<circle class="video_center-cls-1" cx="60.1" cy="60.1" r="58.6"/>
+								
+								<circle class="video_center-cls-2" cx="60.1" cy="60.1" r="58.6"/>
+								
+								<polygon class="video_center-cls-3" points="48.8 44.3 63 52.5 77.3 60.8 63.1 69 48.8 77.3 48.8 60.8 48.8 44.3"/>
+								
+							</svg>
+							
+						</div><!-- single_play_wrapper -->
+					
+					
+					<div class="single_video_inner wistia_embed wistia_async_m8e47nd9p4 popover=true popoverContent=thumbnail">
+						
+						
+						
+					</div><!-- single_video_inner -->
+					
+					<span class="grey_line"></span>
+					
+					<span class="video_title wistia_embed wistia_async_m8e47nd9p4 popover=true popoverContent=html">Lorem Ipsum Amet</span><!-- video_title -->
+					
+					
+				</div><!-- single_video -->
+				
+				
+				
+				
+				<div class="single_video">
+					
+					<div class="single_play_wrapper">
+							
+							<svg class="video_center_play" data-name="Layer 1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 120.2 120.2">
+								
+								<defs><style>.video_center-cls-1,.video_center-cls-3{fill:#fff;}.video_center-cls-1{opacity:0.2;isolation:isolate;}.video_center-cls-2{fill:none;stroke:#fff;stroke-miterlimit:10;stroke-width:3px;}</style></defs>
+								
+								<title>circular-play-button</title>
+								
+								<circle class="video_center-cls-1" cx="60.1" cy="60.1" r="58.6"/>
+								
+								<circle class="video_center-cls-2" cx="60.1" cy="60.1" r="58.6"/>
+								
+								<polygon class="video_center-cls-3" points="48.8 44.3 63 52.5 77.3 60.8 63.1 69 48.8 77.3 48.8 60.8 48.8 44.3"/>
+								
+							</svg>
+							
+						</div><!-- single_play_wrapper -->
+					
+					
+					<div class="single_video_inner wistia_embed wistia_async_m8e47nd9p4 popover=true popoverContent=thumbnail">
+						
+						
+						
+					</div><!-- single_video_inner -->
+					
+					<span class="grey_line"></span>
+					
+					<span class="video_title wistia_embed wistia_async_m8e47nd9p4 popover=true popoverContent=html">Lorem Ipsum Amet</span><!-- video_title -->
+					
+					
+				</div><!-- single_video -->
+				
+				
+				
+				
+				<div class="single_video">
+					
+					<div class="single_play_wrapper">
+							
+							<svg class="video_center_play" data-name="Layer 1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 120.2 120.2">
+								
+								<defs><style>.video_center-cls-1,.video_center-cls-3{fill:#fff;}.video_center-cls-1{opacity:0.2;isolation:isolate;}.video_center-cls-2{fill:none;stroke:#fff;stroke-miterlimit:10;stroke-width:3px;}</style></defs>
+								
+								<title>circular-play-button</title>
+								
+								<circle class="video_center-cls-1" cx="60.1" cy="60.1" r="58.6"/>
+								
+								<circle class="video_center-cls-2" cx="60.1" cy="60.1" r="58.6"/>
+								
+								<polygon class="video_center-cls-3" points="48.8 44.3 63 52.5 77.3 60.8 63.1 69 48.8 77.3 48.8 60.8 48.8 44.3"/>
+								
+							</svg>
+							
+						</div><!-- single_play_wrapper -->
+					
+					
+					<div class="single_video_inner wistia_embed wistia_async_m8e47nd9p4 popover=true popoverContent=thumbnail">
+						
+						
+						
+					</div><!-- single_video_inner -->
+					
+					<span class="grey_line"></span>
+					
+					<span class="video_title wistia_embed wistia_async_m8e47nd9p4 popover=true popoverContent=html">Lorem Ipsum Amet</span><!-- video_title -->
+					
+					
+				</div><!-- single_video -->
+				
+				
+				
+				
+				<div class="single_video">
+					
+					<div class="single_play_wrapper">
+							
+							<svg class="video_center_play" data-name="Layer 1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 120.2 120.2">
+								
+								<defs><style>.video_center-cls-1,.video_center-cls-3{fill:#fff;}.video_center-cls-1{opacity:0.2;isolation:isolate;}.video_center-cls-2{fill:none;stroke:#fff;stroke-miterlimit:10;stroke-width:3px;}</style></defs>
+								
+								<title>circular-play-button</title>
+								
+								<circle class="video_center-cls-1" cx="60.1" cy="60.1" r="58.6"/>
+								
+								<circle class="video_center-cls-2" cx="60.1" cy="60.1" r="58.6"/>
+								
+								<polygon class="video_center-cls-3" points="48.8 44.3 63 52.5 77.3 60.8 63.1 69 48.8 77.3 48.8 60.8 48.8 44.3"/>
+								
+							</svg>
+							
+						</div><!-- single_play_wrapper -->
+					
+					
+					<div class="single_video_inner wistia_embed wistia_async_m8e47nd9p4 popover=true popoverContent=thumbnail">
+						
+						
+						
+					</div><!-- single_video_inner -->
+					
+					<span class="grey_line"></span>
+					
+					<span class="video_title wistia_embed wistia_async_m8e47nd9p4 popover=true popoverContent=html">Lorem Ipsum Amet</span><!-- video_title -->
+					
+					
+				</div><!-- single_video -->
+				
+				
+				
+				
+				<div class="single_video">
+					
+					<div class="single_play_wrapper">
+							
+							<svg class="video_center_play" data-name="Layer 1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 120.2 120.2">
+								
+								<defs><style>.video_center-cls-1,.video_center-cls-3{fill:#fff;}.video_center-cls-1{opacity:0.2;isolation:isolate;}.video_center-cls-2{fill:none;stroke:#fff;stroke-miterlimit:10;stroke-width:3px;}</style></defs>
+								
+								<title>circular-play-button</title>
+								
+								<circle class="video_center-cls-1" cx="60.1" cy="60.1" r="58.6"/>
+								
+								<circle class="video_center-cls-2" cx="60.1" cy="60.1" r="58.6"/>
+								
+								<polygon class="video_center-cls-3" points="48.8 44.3 63 52.5 77.3 60.8 63.1 69 48.8 77.3 48.8 60.8 48.8 44.3"/>
+								
+							</svg>
+							
+						</div><!-- single_play_wrapper -->
+					
+					
+					<div class="single_video_inner wistia_embed wistia_async_m8e47nd9p4 popover=true popoverContent=thumbnail">
+						
+						
+						
+					</div><!-- single_video_inner -->
+					
+					<span class="grey_line"></span>
+					
+					<span class="video_title wistia_embed wistia_async_m8e47nd9p4 popover=true popoverContent=html">Lorem Ipsum Amet</span><!-- video_title -->
+					
+					
+				</div><!-- single_video -->
+				
+				
+				
+				<div class="single_video">
+					
+					<div class="single_play_wrapper">
+							
+							<svg class="video_center_play" data-name="Layer 1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 120.2 120.2">
+								
+								<defs><style></style></defs>
+								
+								<title>circular-play-button</title>
+								
+								<circle class="video_center-cls-1" cx="60.1" cy="60.1" r="58.6"/>
+								
+								<circle class="video_center-cls-2" cx="60.1" cy="60.1" r="58.6"/>
+								
+								<polygon class="video_center-cls-3" points="48.8 44.3 63 52.5 77.3 60.8 63.1 69 48.8 77.3 48.8 60.8 48.8 44.3"/>
+								
+							</svg>
+							
+						</div><!-- single_play_wrapper -->
+					
+					
+					<div class="single_video_inner wistia_embed wistia_async_m8e47nd9p4 popover=true popoverContent=thumbnail">
+						
+						
+						
+					</div><!-- single_video_inner -->
+					
+					<span class="grey_line"></span>
+					
+					<span class="video_title wistia_embed wistia_async_m8e47nd9p4 popover=true popoverContent=html">Lorem Ipsum Amet</span><!-- video_title -->
+					
+					
+				</div><!-- single_video -->
+				
+				
+								
+				
+				
+			</div><!-- videos -->
+			
+			
+			
+			<script src="https://fast.wistia.com/assets/external/E-v1.js" async></script>
+			
+			
+
+			
+			
+		</div><!-- video_center_wrapper -->
+		
+		
+	</div><!-- one_column -->
+	
+	
+</div><!-- internal_main -->
+
+
+
+<?php // get_template_part( 'loop', 'index' );?>
+
+
+
+
+<?php //use get_template_part( 'loop', 'page' ) if needed ?>
+
+	
+
+
+
+<?php get_footer(); ?>
