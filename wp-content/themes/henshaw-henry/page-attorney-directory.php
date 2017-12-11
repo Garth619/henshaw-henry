b43af7891ef5dd47f831884cfe4b9504e43114d5
Garrett Cullen <Garth619@users.noreply.github.com>
2017-12-11 13:28:01 -0800

    push

--- wp-content/themes/henshaw-henry/page-attorney-directory.php
+++ wp-content/themes/henshaw-henry/page-attorney-directory.php
@@ -18,7 +18,7 @@
 					
 					<div class="single_attorney">
 						
-						<a class="" href="">
+						<a class="overlay" href="">
 							
 							<img class="att_image" src="<?php bloginfo('template_directory');?>/images/att-henshaw.jpg"/>
 						
@@ -68,10 +68,61 @@
 						
 					</div><!-- single_attorney -->
 					
+					<div class="single_attorney">
+						
+						<a class="overlay" href="">
+							
+							<img class="att_image" src="<?php bloginfo('template_directory');?>/images/int-att-overview-coming-soon.jpg"/>
+						
+						</a>
+							
+							<div class="atto_title_wrapper">
+								
+								<a class="" href="">
+								
+									<span class="att_overview_title">David S. Henshaw</span><!-- att_overview_title -->
+							
+									<span class="att_overview_position">Founding Partner</span><!-- att_overview_position -->
+									
+								</a>
+								
+							</div><!-- atto_title_wrapper -->
+							
+							<div class="att_social_media">
+								
+								<a class="" href="">
+									
+									<svg class="att_linkedin" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 14.6 14"><title>att-overview-social-linked</title><path class="" d="M3,1.5A1.54,1.54,0,0,1,1.5,3,1.47,1.47,0,0,1,0,1.5,1.54,1.54,0,0,1,1.5,0,1.54,1.54,0,0,1,3,1.5Zm.1,2.8H0V14H3.1Zm4.8,0h-3V14h3V8.9c0-2.9,3.7-3.1,3.7,0V14h3V7.9c0-4.8-5.4-4.6-6.7-2.3Z"/></svg>
+									
+								</a>
+								
+								<a class="" href="">
+									
+									<svg class="att_facebook" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 7.5 16"><defs><style></style></defs><title>att-overview-social-facebook</title>
+									
+									<path id="Facebook" class="att_facebook-cls-1" d="M1.6,3.1V5.3H0V8H1.6v8H5V8H7.2s.2-1.3.3-2.6H5V3.6A.75.75,0,0,1,5.7,3H7.5V.1C6.6,0,5.4,0,5,0,1.5,0,1.6,2.7,1.6,3.1Z"/></svg>
+									
+								</a>
+								
+								<a class="" href="">
+									
+									<svg class="att_google" data-name="Layer 1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 21.5 12.6">
+										
+										<defs><style>s</style></defs><title>att-overview-social-g</title>
+										
+										<path class="att_google-cls-1" d="M6.3,5.4V7.5H9.8a3.36,3.36,0,0,1-3.5,2.7A4,4,0,0,1,2.4,6.3,3.84,3.84,0,0,1,6.3,2.4a3.42,3.42,0,0,1,2.5,1l1.7-1.6A5.6,5.6,0,0,0,6.3,0a6.3,6.3,0,0,0,0,12.6,5.75,5.75,0,0,0,6-6.1,3.08,3.08,0,0,0-.1-1C12.2,5.4,6.3,5.4,6.3,5.4Zm0,0L21.5,7.2H18.8V9.9H17V7.1H14.3V5.4H17V2.7h1.8V5.4h2.7V7.2Z"/></svg>
+									
+								</a>
+								
+							</div><!-- att_social_media -->
+							
+						
+						
+					</div><!-- single_attorney -->
 					
 					<div class="single_attorney">
 						
-						<a class="" href="">
+						<a class="overlay" href="">
 							
 							<img class="att_image" src="<?php bloginfo('template_directory');?>/images/att-henshaw.jpg"/>
 						
@@ -93,25 +144,25 @@
 								
 								<a class="" href="">
 									
-									<svg class="att_linkedin" data-name="Layer 1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 14.6 14"><defs><style>
-										
-										.att_linkedin-cls-1{fill:#103b99;}</style></defs><title>att-overview-social-linked</title><path class="att_linkedin-cls-1" d="M3,1.5A1.54,1.54,0,0,1,1.5,3,1.47,1.47,0,0,1,0,1.5,1.54,1.54,0,0,1,1.5,0,1.54,1.54,0,0,1,3,1.5Zm.1,2.8H0V14H3.1Zm4.8,0h-3V14h3V8.9c0-2.9,3.7-3.1,3.7,0V14h3V7.9c0-4.8-5.4-4.6-6.7-2.3Z"/></svg>
+									<svg class="att_linkedin" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 14.6 14"><title>att-overview-social-linked</title><path class="" d="M3,1.5A1.54,1.54,0,0,1,1.5,3,1.47,1.47,0,0,1,0,1.5,1.54,1.54,0,0,1,1.5,0,1.54,1.54,0,0,1,3,1.5Zm.1,2.8H0V14H3.1Zm4.8,0h-3V14h3V8.9c0-2.9,3.7-3.1,3.7,0V14h3V7.9c0-4.8-5.4-4.6-6.7-2.3Z"/></svg>
 									
 								</a>
 								
 								<a class="" href="">
 									
-									<svg class="att_linkedin" data-name="Layer 1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 14.6 14"><defs><style>
-										
-										.att_linkedin-cls-1{fill:#103b99;}</style></defs><title>att-overview-social-linked</title><path class="att_linkedin-cls-1" d="M3,1.5A1.54,1.54,0,0,1,1.5,3,1.47,1.47,0,0,1,0,1.5,1.54,1.54,0,0,1,1.5,0,1.54,1.54,0,0,1,3,1.5Zm.1,2.8H0V14H3.1Zm4.8,0h-3V14h3V8.9c0-2.9,3.7-3.1,3.7,0V14h3V7.9c0-4.8-5.4-4.6-6.7-2.3Z"/></svg>
+									<svg class="att_facebook" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 7.5 16"><defs><style></style></defs><title>att-overview-social-facebook</title>
+									
+									<path id="Facebook" class="att_facebook-cls-1" d="M1.6,3.1V5.3H0V8H1.6v8H5V8H7.2s.2-1.3.3-2.6H5V3.6A.75.75,0,0,1,5.7,3H7.5V.1C6.6,0,5.4,0,5,0,1.5,0,1.6,2.7,1.6,3.1Z"/></svg>
 									
 								</a>
 								
 								<a class="" href="">
 									
-									<svg class="att_linkedin" data-name="Layer 1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 14.6 14"><defs><style>
+									<svg class="att_google" data-name="Layer 1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 21.5 12.6">
 										
-										.att_linkedin-cls-1{fill:#103b99;}</style></defs><title>att-overview-social-linked</title><path class="att_linkedin-cls-1" d="M3,1.5A1.54,1.54,0,0,1,1.5,3,1.47,1.47,0,0,1,0,1.5,1.54,1.54,0,0,1,1.5,0,1.54,1.54,0,0,1,3,1.5Zm.1,2.8H0V14H3.1Zm4.8,0h-3V14h3V8.9c0-2.9,3.7-3.1,3.7,0V14h3V7.9c0-4.8-5.4-4.6-6.7-2.3Z"/></svg>
+										<defs><style>s</style></defs><title>att-overview-social-g</title>
+										
+										<path class="att_google-cls-1" d="M6.3,5.4V7.5H9.8a3.36,3.36,0,0,1-3.5,2.7A4,4,0,0,1,2.4,6.3,3.84,3.84,0,0,1,6.3,2.4a3.42,3.42,0,0,1,2.5,1l1.7-1.6A5.6,5.6,0,0,0,6.3,0a6.3,6.3,0,0,0,0,12.6,5.75,5.75,0,0,0,6-6.1,3.08,3.08,0,0,0-.1-1C12.2,5.4,6.3,5.4,6.3,5.4Zm0,0L21.5,7.2H18.8V9.9H17V7.1H14.3V5.4H17V2.7h1.8V5.4h2.7V7.2Z"/></svg>
 									
 								</a>
 								
@@ -121,10 +172,9 @@
 						
 					</div><!-- single_attorney -->
 					
-					
 					<div class="single_attorney">
 						
-						<a class="" href="">
+						<a class="overlay" href="">
 							
 							<img class="att_image" src="<?php bloginfo('template_directory');?>/images/att-henshaw.jpg"/>
 						
@@ -146,25 +196,25 @@
 								
 								<a class="" href="">
 									
-									<svg class="att_linkedin" data-name="Layer 1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 14.6 14"><defs><style>
-										
-										.att_linkedin-cls-1{fill:#103b99;}</style></defs><title>att-overview-social-linked</title><path class="att_linkedin-cls-1" d="M3,1.5A1.54,1.54,0,0,1,1.5,3,1.47,1.47,0,0,1,0,1.5,1.54,1.54,0,0,1,1.5,0,1.54,1.54,0,0,1,3,1.5Zm.1,2.8H0V14H3.1Zm4.8,0h-3V14h3V8.9c0-2.9,3.7-3.1,3.7,0V14h3V7.9c0-4.8-5.4-4.6-6.7-2.3Z"/></svg>
+									<svg class="att_linkedin" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 14.6 14"><title>att-overview-social-linked</title><path class="" d="M3,1.5A1.54,1.54,0,0,1,1.5,3,1.47,1.47,0,0,1,0,1.5,1.54,1.54,0,0,1,1.5,0,1.54,1.54,0,0,1,3,1.5Zm.1,2.8H0V14H3.1Zm4.8,0h-3V14h3V8.9c0-2.9,3.7-3.1,3.7,0V14h3V7.9c0-4.8-5.4-4.6-6.7-2.3Z"/></svg>
 									
 								</a>
 								
 								<a class="" href="">
 									
-									<svg class="att_linkedin" data-name="Layer 1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 14.6 14"><defs><style>
-										
-										.att_linkedin-cls-1{fill:#103b99;}</style></defs><title>att-overview-social-linked</title><path class="att_linkedin-cls-1" d="M3,1.5A1.54,1.54,0,0,1,1.5,3,1.47,1.47,0,0,1,0,1.5,1.54,1.54,0,0,1,1.5,0,1.54,1.54,0,0,1,3,1.5Zm.1,2.8H0V14H3.1Zm4.8,0h-3V14h3V8.9c0-2.9,3.7-3.1,3.7,0V14h3V7.9c0-4.8-5.4-4.6-6.7-2.3Z"/></svg>
+									<svg class="att_facebook" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 7.5 16"><defs><style></style></defs><title>att-overview-social-facebook</title>
+									
+									<path id="Facebook" class="att_facebook-cls-1" d="M1.6,3.1V5.3H0V8H1.6v8H5V8H7.2s.2-1.3.3-2.6H5V3.6A.75.75,0,0,1,5.7,3H7.5V.1C6.6,0,5.4,0,5,0,1.5,0,1.6,2.7,1.6,3.1Z"/></svg>
 									
 								</a>
 								
 								<a class="" href="">
 									
-									<svg class="att_linkedin" data-name="Layer 1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 14.6 14"><defs><style>
+									<svg class="att_google" data-name="Layer 1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 21.5 12.6">
 										
-										.att_linkedin-cls-1{fill:#103b99;}</style></defs><title>att-overview-social-linked</title><path class="att_linkedin-cls-1" d="M3,1.5A1.54,1.54,0,0,1,1.5,3,1.47,1.47,0,0,1,0,1.5,1.54,1.54,0,0,1,1.5,0,1.54,1.54,0,0,1,3,1.5Zm.1,2.8H0V14H3.1Zm4.8,0h-3V14h3V8.9c0-2.9,3.7-3.1,3.7,0V14h3V7.9c0-4.8-5.4-4.6-6.7-2.3Z"/></svg>
+										<defs><style>s</style></defs><title>att-overview-social-g</title>
+										
+										<path class="att_google-cls-1" d="M6.3,5.4V7.5H9.8a3.36,3.36,0,0,1-3.5,2.7A4,4,0,0,1,2.4,6.3,3.84,3.84,0,0,1,6.3,2.4a3.42,3.42,0,0,1,2.5,1l1.7-1.6A5.6,5.6,0,0,0,6.3,0a6.3,6.3,0,0,0,0,12.6,5.75,5.75,0,0,0,6-6.1,3.08,3.08,0,0,0-.1-1C12.2,5.4,6.3,5.4,6.3,5.4Zm0,0L21.5,7.2H18.8V9.9H17V7.1H14.3V5.4H17V2.7h1.8V5.4h2.7V7.2Z"/></svg>
 									
 								</a>
 								
@@ -174,7 +224,111 @@
 						
 					</div><!-- single_attorney -->
 					
+					<div class="single_attorney">
+						
+						<a class="overlay" href="">
+							
+							<img class="att_image" src="<?php bloginfo('template_directory');?>/images/att-henshaw.jpg"/>
+						
+						</a>
+							
+							<div class="atto_title_wrapper">
+								
+								<a class="" href="">
+								
+									<span class="att_overview_title">David S. Henshaw</span><!-- att_overview_title -->
+							
+									<span class="att_overview_position">Founding Partner</span><!-- att_overview_position -->
+									
+								</a>
+								
+							</div><!-- atto_title_wrapper -->
+							
+							<div class="att_social_media">
+								
+								<a class="" href="">
+									
+									<svg class="att_linkedin" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 14.6 14"><title>att-overview-social-linked</title><path class="" d="M3,1.5A1.54,1.54,0,0,1,1.5,3,1.47,1.47,0,0,1,0,1.5,1.54,1.54,0,0,1,1.5,0,1.54,1.54,0,0,1,3,1.5Zm.1,2.8H0V14H3.1Zm4.8,0h-3V14h3V8.9c0-2.9,3.7-3.1,3.7,0V14h3V7.9c0-4.8-5.4-4.6-6.7-2.3Z"/></svg>
+									
+								</a>
+								
+								<a class="" href="">
+									
+									<svg class="att_facebook" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 7.5 16"><defs><style></style></defs><title>att-overview-social-facebook</title>
+									
+									<path id="Facebook" class="att_facebook-cls-1" d="M1.6,3.1V5.3H0V8H1.6v8H5V8H7.2s.2-1.3.3-2.6H5V3.6A.75.75,0,0,1,5.7,3H7.5V.1C6.6,0,5.4,0,5,0,1.5,0,1.6,2.7,1.6,3.1Z"/></svg>
+									
+								</a>
+								
+								<a class="" href="">
+									
+									<svg class="att_google" data-name="Layer 1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 21.5 12.6">
+										
+										<defs><style>s</style></defs><title>att-overview-social-g</title>
+										
+										<path class="att_google-cls-1" d="M6.3,5.4V7.5H9.8a3.36,3.36,0,0,1-3.5,2.7A4,4,0,0,1,2.4,6.3,3.84,3.84,0,0,1,6.3,2.4a3.42,3.42,0,0,1,2.5,1l1.7-1.6A5.6,5.6,0,0,0,6.3,0a6.3,6.3,0,0,0,0,12.6,5.75,5.75,0,0,0,6-6.1,3.08,3.08,0,0,0-.1-1C12.2,5.4,6.3,5.4,6.3,5.4Zm0,0L21.5,7.2H18.8V9.9H17V7.1H14.3V5.4H17V2.7h1.8V5.4h2.7V7.2Z"/></svg>
+									
+								</a>
+								
+							</div><!-- att_social_media -->
+							
+						
+						
+					</div><!-- single_attorney -->
 					
+					<div class="single_attorney">
+						
+						<a class="overlay" href="">
+							
+							<img class="att_image" src="<?php bloginfo('template_directory');?>/images/att-henshaw.jpg"/>
+						
+						</a>
+							
+							<div class="atto_title_wrapper">
+								
+								<a class="" href="">
+								
+									<span class="att_overview_title">David S. Henshaw</span><!-- att_overview_title -->
+							
+									<span class="att_overview_position">Founding Partner</span><!-- att_overview_position -->
+									
+								</a>
+								
+							</div><!-- atto_title_wrapper -->
+							
+							<div class="att_social_media">
+								
+								<a class="" href="">
+									
+									<svg class="att_linkedin" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 14.6 14"><title>att-overview-social-linked</title><path class="" d="M3,1.5A1.54,1.54,0,0,1,1.5,3,1.47,1.47,0,0,1,0,1.5,1.54,1.54,0,0,1,1.5,0,1.54,1.54,0,0,1,3,1.5Zm.1,2.8H0V14H3.1Zm4.8,0h-3V14h3V8.9c0-2.9,3.7-3.1,3.7,0V14h3V7.9c0-4.8-5.4-4.6-6.7-2.3Z"/></svg>
+									
+								</a>
+								
+								<a class="" href="">
+									
+									<svg class="att_facebook" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 7.5 16"><defs><style></style></defs><title>att-overview-social-facebook</title>
+									
+									<path id="Facebook" class="att_facebook-cls-1" d="M1.6,3.1V5.3H0V8H1.6v8H5V8H7.2s.2-1.3.3-2.6H5V3.6A.75.75,0,0,1,5.7,3H7.5V.1C6.6,0,5.4,0,5,0,1.5,0,1.6,2.7,1.6,3.1Z"/></svg>
+									
+								</a>
+								
+								<a class="" href="">
+									
+									<svg class="att_google" data-name="Layer 1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 21.5 12.6">
+										
+										<defs><style>s</style></defs><title>att-overview-social-g</title>
+										
+										<path class="att_google-cls-1" d="M6.3,5.4V7.5H9.8a3.36,3.36,0,0,1-3.5,2.7A4,4,0,0,1,2.4,6.3,3.84,3.84,0,0,1,6.3,2.4a3.42,3.42,0,0,1,2.5,1l1.7-1.6A5.6,5.6,0,0,0,6.3,0a6.3,6.3,0,0,0,0,12.6,5.75,5.75,0,0,0,6-6.1,3.08,3.08,0,0,0-.1-1C12.2,5.4,6.3,5.4,6.3,5.4Zm0,0L21.5,7.2H18.8V9.9H17V7.1H14.3V5.4H17V2.7h1.8V5.4h2.7V7.2Z"/></svg>
+									
+								</a>
+								
+							</div><!-- att_social_media -->
+							
+						
+						
+					</div><!-- single_attorney -->
+					
+
 					
 				</div><!-- attorney_overvew -->
 				
