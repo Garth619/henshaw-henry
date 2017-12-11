e86e52621112d3f7ed8a92ac2d900a30badb9a05
Garrett Cullen <Garth619@users.noreply.github.com>
2017-12-11 14:23:20 -0800

    push

---
+++ wp-content/themes/henshaw-henry/page-aboutoutfirm.php
@@ -0,0 +1,66 @@
+<?php 
+	
+	/* Template Name: About Our Firm */
+	
+	get_header(); ?>
+
+
+
+<div class="internal_main">
+	
+	
+	<div class="about_wrapper">
+		
+		
+		<div class="about_content">
+			
+			<div class="about_content_inner content">
+				
+				<h1>About Our Firm</h1>
+				
+				<h2>Henshaw & Henry, PC in San Jose, California, serves clients throughout the Bay Area.</h2>
+				
+				<p>At Henshaw & Henry, PC, we provide high quality legal services to each of our clients. 
+We are experienced in various legal matters including: civil litigation, estate planning, probate, administrative law, and alternative dispute resolution. We manage business transactions, negotiate successful resolutions, and advise clients to keep them out of court.</p>
+
+				<p>Henshaw & Henry, PC is knowledgeable, well-rounded, and versatile. We are experienced in the common problems that individuals and businesses face. While each and every client has a unique set of problems, we are confident that we have the expertise to resolve your legal issues.</p>
+				
+				<div class="about_page_blockquote">
+					
+					<div class="about_blockquote_img_wrapper">
+						
+						<img src="<?php bloginfo('template_directory');?>/images/int-about-cal-bear-icon.svg"/>
+						
+					</div><!-- about_blockquote_img_wrapper -->
+					
+					<div class="about_blockquote_text_wrapper">
+						
+						We enjoy building relationships with our clients and understand the problems they face every day.
+						
+					</div><!-- about_blockquote_text_wrapper -->
+				
+				</div><!-- about_page_blockquote -->
+				
+				<p>Too often people are afraid to contact their lawyer. Our clients appreciate that we listen to their goals and objectives during the entire representation. In doing so, we are able to advise them towards the most desired legal conclusion.</p>
+
+				<p>Henshaw & Henry, PC in San Jose, California, serves its clients throughout San Jose and San Francisco Bay Area in the areas of civil litigation, including real property law, employment law, contract law, personal injury and construction law, estate planning, probate litigation, and trust administration.</p>
+				
+				
+					
+				
+				
+			</div><!-- about_content_inner -->
+			
+		</div><!-- about_content -->
+		
+		<div class="about_sidebar"></div><!-- about_sidebar -->
+		
+		
+	</div><!-- about_wrapper -->
+	
+	
+</div><!-- internal_main -->
+
+
+
+<?php get_footer(); ?>
