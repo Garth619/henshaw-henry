6afa3ff7316215c6ca89747d75b1b1587e987638
Garrett Cullen <Garth619@users.noreply.github.com>
2017-12-11 16:27:53 -0800

    push

---
+++ wp-content/themes/henshaw-henry/sidebar-bio.php
@@ -0,0 +1,55 @@
+
+<div class="sidebar_bio">
+	
+	<img class="attor_profile_img" src="<?php bloginfo('template_directory');?>/images/att-henshaw.jpg"/>
+	
+	<ul>
+		<li>Education
+			
+			<ul>
+				
+				<li>JD  – University of the Pacific, McGeorge School of Law, With Honors, Certificate in International Legal Studies</li>
+				
+				<li>MBA – Indiana University, Kelley Schoolof Business</li>
+				
+				<li>BA, Economics – California State University, Fullerton</li>
+			
+			</ul>
+		
+		</li>
+		<li>Education
+			
+			<ul>
+				
+				<li>JD  – University of the Pacific, McGeorge School of Law, With Honors, Certificate in International Legal Studies</li>
+				
+				<li>MBA – Indiana University, Kelley Schoolof Business</li>
+				
+				<li>BA, Economics – California State University, Fullerton</li>
+			
+			</ul>
+		
+		</li>
+		<li>Education
+			
+			<ul>
+				
+				<li>JD  – University of the Pacific, McGeorge School of Law, With Honors, Certificate in International Legal Studies</li>
+				
+				<li>MBA – Indiana University, Kelley Schoolof Business</li>
+				
+				<li>BA, Economics – California State University, Fullerton</li>
+			
+			</ul>
+		
+		</li>
+	</ul>
+	
+	
+	
+	
+	
+</div><!-- sidebar -->
+
+
+			
